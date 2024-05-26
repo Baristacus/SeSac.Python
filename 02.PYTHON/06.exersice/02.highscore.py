1f1f24450711dfc9d5e4878f5eef316b91abafa7
Baristacus <chic.igel@gmail.com>
2024-05-27 02:20:25 +0900

    숙제

--- 02.PYTHON/06.exersice/02.highscore.py
+++ 02.PYTHON/06.exersice/02.highscore.py
@@ -1,85 +1,167 @@
 """하이스코어
 점수와 이름을 입력받고, 다양한 모드(score, high, history)에 따라서
 각각, "스코어입력", "하이스코어출력", "전체히스토리보기" 기능 구현
-
 ----------------------------------------------------------
-
-1. 실행 시 메뉴를 보여주고, 번호를 선택한다.
-2. 
-2. 저장을 한다.(txt? csv? 어디다가?)
-    - 기존에 파일이 없다면 만든다.
-    - 파일이 있다면?
-3. 
+1. csv파일은 현재 경로에 저장
+    - 파일이 없는경우 실행 시 새로 만듦
+2. 메인메뉴 보여주고, 번호 선택하기
+3. 해당 메뉴에 따라 화면 구성하기
+    - 스코어입력 : 이름과 스코어를 입력받아 저장(순번은 자동증가)
+    - 하이스코어출력 : 기록된 목록에서 TOP3 출력
+    - 전체히스토리 : 저장된 모든 기록 출력
 """
 
 import csv
+import os
 
-file_path = "02.highscore.csv"
+current_file_path = os.path.abspath(__file__)
+current_folder = os.path.dirname(current_file_path)
+
+file_path = os.path.join(current_folder, "02.highscore.csv")
+data_list = []
+
+
+def create_file(file_path):
+    if not os.path.exists(file_path):
+        with open(file_path, "w", encoding="UTF-8", newline="") as file:
+            headers = ["No", "Name", "Score"]
+            score_save = csv.DictWriter(file, fieldnames=headers)
+            score_save.writeheader()
 
 
 def score_input():
-    auto_num = 1
-    input_name = input("이름을 입력하세요: ")
-    input_score = input("스코어를 입력하세요: ")
+    with open(file_path, "r", encoding="utf-8") as f:
+        all_list = csv.reader(f)
+        for row in all_list:
+            data_list.append(row)
+
+    auto_num = (len(data_list) - 1) + 1
+
+    while True:
+        input_name = input("이름을 입력하세요: ")
+        if input_name.strip() and not input_name.isdigit():
+            break
+        else:
+            print("올바른 이름을 입력해주세요.")
+
+    while True:
+        input_score = input("스코어를 입력하세요: ")
+        if input_score.isdigit():
+            break
+        else:
+            print("스코어는 숫자로 입력해주세요.")
+
+    input_data = {"No": auto_num, "Name": input_name, "Score": input_score}
 
     with open(file_path, "a", encoding="UTF-8", newline="") as file:
         headers = ["No", "Name", "Score"]
         score_save = csv.DictWriter(file, fieldnames=headers)
-        score_save.writeheader()
-        score_save.writerow([auto_num, input_name, input_score])
-    return print("저장완료")
+        if len(data_list) == 0:
+            score_save.writeheader()
+        score_save.writerow(input_data)
+
+    data_list.clear()
+    return print("  [저장완료]")
 
 
-def high_output():
-    return
+def high_score():
+    with open(file_path, "r", encoding="utf-8") as file:
+        all_list = csv.DictReader(file)
+        for row in all_list:
+            data_list.append(row)
+
+    sorted_list = sorted(data_list, key=lambda x: int(x["Score"]), reverse=True)
+
+    rank_num = 0
+
+    for aaa in sorted_list[:3]:
+        rank_num += 1
+        print(f'  {rank_num}. {aaa["Name"]} - {aaa["Score"]}')
+
+    data_list.clear()
+    return print("  [불러오기 완료]")
 
 
 def score_history():
-    return
+    with open(file_path, "r", encoding="UTF-8") as file:
+        score_list = csv.DictReader(file)
+        if not score_list:
+            print("  [데이터가 없습니다.]")
+        else:
+            for data in score_list:
+                print(f'  {data["No"]}. {data["Name"]} - {data["Score"]}')
+
+    return print("  [불러오기 완료]")
 
 
 def menu(num):
     if num == 0:
         print(
             "\n\n\n\n\n"
-            "*=================================================*\n"
+            "=================================================\n"
             "  번호를 선택 후 엔터(↵)를 입력해주세요.\n"
-            "*-------------------------------------------------*\n"
+            "-------------------------------------------------\n"
             "  1. 스코어 입력\n"
-            "  2. 하이스코어 출력\n"
-            "  3. 전체히스토리\n"
-            "\n"
-            "*-------------------------------------------------*\n"
-            "  0: 메인메뉴 | 9: 종료\n"
-            "*=================================================*"
+            "  2. 랭킹 TOP3\n"
+            "  3. 전체목록\n"
+            "-------------------------------------------------\n"
+            "  9: 종료\n"
+            "================================================="
         )
         num = int(input("번호입력: "))
         menu(num)
     if num == 1:
         print(
             "\n\n\n\n\n"
-            "*=================================================*\n"
-            "  번호를 선택 후 엔터(↵)를 입력해주세요.\n"
-            "*-------------------------------------------------*\n"
+            "=================================================\n"
             "  스코어 입력\n"
+            "-------------------------------------------------"
         )
-        # 여기에... 스코어 입력 함수 호출
         score_input()
         print(
-            "*-------------------------------------------------*\n"
+            "-------------------------------------------------\n"
             "  0: 메인메뉴 | 9: 종료\n"
-            "*=================================================*"
+            "================================================="
+        )
+        num = int(input("번호입력: "))
+        menu(num)
+    if num == 2:
+        print(
+            "\n\n\n\n\n"
+            "=================================================\n"
+            "  랭킹 TOP3\n"
+            "-------------------------------------------------"
+        )
+        high_score()
+        print(
+            "-------------------------------------------------\n"
+            "  0: 메인메뉴 | 9: 종료\n"
+            "================================================="
+        )
+        num = int(input("번호입력: "))
+        menu(num)
+    if num == 3:
+        print(
+            "\n\n\n\n\n"
+            "=================================================\n"
+            "  전체목록\n"
+            "-------------------------------------------------"
+        )
+        score_history()
+        print(
+            "-------------------------------------------------\n"
+            "  0: 메인메뉴 | 9: 종료\n"
+            "================================================="
         )
         num = int(input("번호입력: "))
         menu(num)
     if num == 9:
         exit(1)
-    return num
+
+    if not num is 0 or 1 or 2 or 3 or 9:
+        print("올바른번호를 입력해주세요.")
+        menu(0)
 
 
+create_file(file_path)
 menu(0)
-
-
-# if menu_num == "1":
-#     input_name = input("이름을 입력하세요: ")
-#     input_score = input("스코어를 입력하세요: ")
--- 02.PYTHON/06.exersice/03.calendar.py
+++ 02.PYTHON/06.exersice/03.calendar.py
@@ -10,15 +10,70 @@
 
 # -------------------------------------------
 """ 달력 함수를 사용하지 않고 달력 구현하기
-1.
+1. 월의 마지막 날짜 계산
+    - 2월 : 28일, 29일
+    - 1월, 3월, 5월, 7월, 8월, 10월, 12월 : 31일
+    - 4월, 6월, 9월, 11월 : 30일
+2. 윤년, 평년 계산
+    - 4로 나누어 떨어지면서 400으로 나누어 떨어지는 해는 윤년
+    - 4로 나누어 떨어지지만 100으로 나누어 떨어지는 해는 윤년이 아님
+3. 시작요일 계산
+    - Zeller 공식(1월과 2월은 연도 -1 월 + 1)
+        - h = (1 + (26 * (month + 1)) // 10 + year + year // 4 - year // 100 + year // 400) % 7
+        - 0: 토요일, 1: 일요일, 2: 월요일, 3: 화요일, 4: 수요일, 5: 목요일, 6: 금요일
+4. 년도와 월을 입력 받아 출력
 """
 
 
-def my_cal(year, month):
-    for i in range(1, 30):
-        print(f"{i}", end="")
-        if i % 7 == 0:
+def last_day(year, month):
+    if month == 2:
+        if year % 4 == 0 and year % 400 == 0 or year % 100 != 0:
+            return 29
+        else:
+            return 28
+    elif month in [1, 3, 5, 7, 8, 10, 12]:
+        return 31
+    else:
+        return 30
+
+
+def day_of_week(year, month):
+    if month == 1 or month == 2:
+        year -= 1
+        month += 12
+    week = (
+        1 + (26 * (month + 1)) // 10 + year + year // 4 - year // 100 + year // 400
+    ) % 7
+    return week
+
+
+def line_print(count, line, end=""):
+    print(count * line, end)
+
+
+def my_cal():
+    year = int(input("연도를 입력하세요: "))
+    month = int(input("월를 입력하세요: "))
+    start_week = day_of_week(year, month)
+
+    line_print(30, "=")
+    print(f"{year}년 {month}월 달력")
+    line_print(30, "-")
+    print("  일  월  화  수  목  금  토")
+
+    padding = "    " * (start_week - 1)
+
+    for i in range(1, last_day(year, month) + 1):
+        if i == 1:
+            print(padding, end="")
+
+        print(f" {i:2d} ", end="")
+
+        if (i + start_week - 1) % 7 == 0:
             print("")
 
+    print("")
+    line_print(30, "=")
 
-my_cal(2025, 1)
+
+my_cal()
